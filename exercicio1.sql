30b33b1f8a5e9ba7169949b808aa0e5f04c19094
JacqueADev <jacquelineadev@gmail.com>
2024-12-07 00:35:38 -0300

    Create exercicio1.sql
    Faça uma lista com todos os produtos das cores: Green, Orange, Black, Silver e Pink. Estes produtos devem ser exclusivamente das marcas: Contoso, Litware e Fabrikam.

---
+++ exercicio1.sql
@@ -0,0 +1,12 @@
+/*Faça uma lista com todos os produtos das cores: Green, Orange, Black, Silver e Pink. Estes produtos devem ser exclusivamente das marcas: Contoso, Litware e Fabrikam.*/
+
+SELECT * FROM DimProduct
+
+SELECT
+	ProductName,
+	ColorName,
+	BrandName
+FROM
+	DimProduct
+
+WHERE ColorName IN ('Green', 'Orange', 'Black', 'Silver', 'Pink') AND  BrandName IN ( 'Contoso', 'Litware', 'Fabrikam')
